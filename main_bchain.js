5b9f6b77f04da584766d72988a38aa17c746352e
RichA <rich_ay@sbcglobal.net>
2023-01-26 15:32:07 -0600

    Added additional data to each sample block.

--- main_bchain.js
+++ main_bchain.js
@@ -15,13 +15,13 @@ let timestamp = 0;
 
 // Add a few more blocks (note we make up the data here).
 timestamp = moment().format();
-bc.addBlock( new Block( 1, timestamp, 0, {data_amount: 4}  ) );
+bc.addBlock( new Block( 1, timestamp, 0, {data_amount: 4, user: 'tom'}  ) );
 
 timestamp = moment().format();
-bc.addBlock( new Block( 2, timestamp, 0, {data_amount: 14} ) );
+bc.addBlock( new Block( 2, timestamp, 0, {data_amount: 14, user: 'dick'} ) );
 
 timestamp = moment().format();
-bc.addBlock( new Block( 3, timestamp, 0, {data_amount: 24} ) );
+bc.addBlock( new Block( 3, timestamp, 0, {data_amount: 24, user: 'harry'} ) );
 
 // Dump out the chain of blocks to the console
 console.log( JSON.stringify( bc, null, 4 ) );
@@ -31,7 +31,7 @@ var valid = bc.isChainValid();
 console.log( "Is the chain valid?  " + valid );
 
 // Alter the data in block 2, to invalidate the chain.
-bc.chain[2].blockData = {data_amount: 15};
+bc.chain[2].blockData = {data_amount: 15, user: 'hacker'};
 bc.chain[2].hash      = bc.chain[2].calculateHash();   // recalculate the hash, attempting to cover up the data change
 
 console.log( " " );
